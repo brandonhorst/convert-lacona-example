ce75281c767ad64663d9a48f80172ae137b69ffb
Brandon Horst <brandonhorst@gmail.com>
2016-10-20 13:48:21 +0800

    Add imageRoot parameter

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 var _ = require('lodash')
 var markdownToAST = require('markdown-to-ast').parse
 var escapeHTML = require('escape-html')
+var join = require('path').join
 
-module.exports = function convert (example) {
+module.exports = function convert (example, imageRoot = '') {
   var ast = markdownToAST(example)
-  return parseNode(ast)
+  return parseNode(ast, imageRoot)
 }
 
-function mapParseNode (children) {
-  return _.chain(children).map(parseNode).join('').value()
+function mapParseNode (children, imageRoot) {
+  return _.chain(children).map(child => parseNode(child, imageRoot)).join('').value()
 }
 
-function parseNode (node) {
+function parseNode (node, imageRoot) {
   switch (node.type) {
     case 'Str':
       return '<span class="string">' + escapeHTML(node.value) + '</span>'
     case 'Image':
-      return '<img class="image" src="' + escapeHTML(node.url) + '" />'
+      return '<img class="image" src="' + escapeHTML(join(imageRoot, node.url)) + '" />'
     case 'Emphasis':
-      return '<span class="category-symbol">' + mapParseNode(node.children) + '</span>'
-    // case 'Strong':
-    //   return '<span class="placeholder ">' + mapParseNode(node.children) + '</span>'
+      return '<span class="category-symbol">' + mapParseNode(node.children, imageRoot) + '</span>'
     case 'Link':
       const urlClass = escapeHTML(_.kebabCase(node.url))
       if (node.children.length) {
         return (
           '<span class="argumentSegment argument-' + urlClass + '">' + 
             '<span class="argument">' + escapeHTML(node.url) + '</span>' +
-            '<span class="content">' + mapParseNode(node.children) + '</span>' +
+            '<span class="content">' + mapParseNode(node.children, imageRoot) + '</span>' +
           '</span>'
         )
       } else {
         return '<span class="placeholder argument-' + urlClass + '">' + escapeHTML(node.url) + '</span>'
       }
     case 'Paragraph':
-      return '<div class="example">' + mapParseNode(node.children) + '</div>'
+      return '<div class="example">' + mapParseNode(node.children, imageRoot) + '</div>'
     case 'Document':
     default:
-      return mapParseNode(node.children)
+      return mapParseNode(node.children, imageRoot)
   }
 }
--- test/index.js
+++ test/index.js
@@ -7,4 +7,10 @@ describe('convert', function () {
     var html = convert(example)
     expect(html).to.equal('<div class="example"><span class="string">open </span><img class="image" src="img/clipboard.png" /><span class="argumentSegment argument-clipboard-url"><span class="argument">clipboard url</span><span class="content"><span class="category-symbol"><span class="string">clipboard</span></span></span></span><span class="string"> in </span><span class="placeholder argument-application">application</span></div>')
   })
+
+  it('converts with imageRoot', function () {
+    var example = 'open ![](img/clipboard.png)[*clipboard*](clipboard url) in [](application)'
+    var html = convert(example, '/tmp')
+    expect(html).to.equal('<div class="example"><span class="string">open </span><img class="image" src="/tmp/img/clipboard.png" /><span class="argumentSegment argument-clipboard-url"><span class="argument">clipboard url</span><span class="content"><span class="category-symbol"><span class="string">clipboard</span></span></span></span><span class="string"> in </span><span class="placeholder argument-application">application</span></div>')
+  })
 })
